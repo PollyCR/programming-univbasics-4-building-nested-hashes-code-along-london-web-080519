7153837772e08acaeb03701f8b980b33a7782cbc
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-11 21:15:16 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,16 +1,16 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => ,
-      :matriarch => ,
-      :hero => "Romeo",
-      :hero_friends => ,
+      :patriarch => 
+      :matriarch => 
+      :hero => "Romeo"
+      :hero_friends => 
       }, 
     :capulet => {
-      :patriarch => ,
-      :matriarch => ,
-      :heroine => "Juliet",
-      :heroine_friends => ,
+      :patriarch => 
+      :matriarch => 
+      :heroine => "Juliet"
+      :heroine_friends => 
       }}
 
   
